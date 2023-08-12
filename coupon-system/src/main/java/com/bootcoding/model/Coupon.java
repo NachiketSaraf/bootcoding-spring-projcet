c7d18f269d813a992c34bb7b41590de179a2f991
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-13 01:35:30 +0530

    Coupon

---
+++ coupon-system/src/main/java/com/bootcoding/model/Coupon.java
@@ -0,0 +1,12 @@
+package com.bootcoding.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Coupon {
+
+    public String name(){
+        return "Nachiket";
+    }
+}
