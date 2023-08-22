e299707dc93fdb7ff09a81643f561909aefb69d0
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-22 15:28:20 +0530

    Patient Repository

--- Patient/src/main/java/com/bootcoding/patient/repository/PatientRepository.java
+++ Patient/src/main/java/com/bootcoding/patient/repository/PatientRepository.java
@@ -1,7 +1,10 @@
 package com.bootcoding.patient.repository;
 
+import com.bootcoding.patient.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class PatientRepository {
+public interface PatientRepository extends JpaRepository<Patient,Integer> {
+
 }
