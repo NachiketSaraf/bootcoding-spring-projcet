e0006218902458f240b36d201ea5626f741e26b1
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-22 15:23:52 +0530

    Patient

--- Patient/src/main/java/com/bootcoding/patient/model/Patient.java
+++ Patient/src/main/java/com/bootcoding/patient/model/Patient.java
@@ -1,14 +1,34 @@
 package com.bootcoding.patient.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
+@Table (name = "patient")
+@Entity
 public class Patient {
+
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column(name = "first_name")
     private String name;
+
+    @Column(name = "age")
     private int age;
+
+    @Column(name = "disease")
     private String disease;
+
+    @Column(name = "phone_no")
     private String phoneNo;
 }
+
