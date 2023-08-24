3e4572abe82d20516f6e270b9680867a0f491e52
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-24 20:16:52 +0530

    Random CreatedDateGenerator

---
+++ coupon-system/coupon-system/src/main/java/com/bootcoding/spring/coupon/utils/CreatedDateGenerator.java
@@ -0,0 +1,11 @@
+package com.bootcoding.spring.coupon.utils;
+
+import java.util.Date;
+
+public class CreatedDateGenerator {
+    public static Date createdDate(){
+        Date date = new Date();
+         date.get;
+    }
+
+}
