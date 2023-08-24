32d3058e1bc0797693539e56d6d0b318ffdb57e0
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-24 10:26:52 +0530

    Random Categories Generator

---
+++ coupon-system/coupon-system/src/main/java/com/bootcoding/spring/coupon/utils/CategoriesGenerator.java
@@ -0,0 +1,34 @@
+package com.bootcoding.spring.coupon.utils;
+
+import java.util.Random;
+
+public class CategoriesGenerator {
+    private static String[] couponCategories = {
+            "Apparel and Fashion",
+            "Electronics",
+            "Home and Living",
+            "Food and Dining",
+            "Travel and Hospitality",
+            "Beauty and Personal Care",
+            "Health and Fitness",
+            "Entertainment",
+            "Automotive",
+            "Books and Stationery",
+            "Gifts and Flowers",
+            "Sports and Outdoors",
+            "Kids and Toys",
+            "Pets",
+            "Tech Services",
+            "Subscription Services",
+            "Special Occasions",
+            "Local Services",
+            "Exclusive/VIP",
+            "Clearance",
+            "Seasonal"
+    };
+    public String getCategories(){
+        Random random = new Random();
+        return couponCategories[random.nextInt(0,couponCategories.length-1)];
+    }
+
+}
