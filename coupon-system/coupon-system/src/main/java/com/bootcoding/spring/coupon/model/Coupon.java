3e07f3c615549e626eeff3e5505543604a9ecbf0
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-25 19:07:05 +0530

    coupon model

---
+++ coupon-system/coupon-system/src/main/java/com/bootcoding/spring/coupon/model/Coupon.java
@@ -0,0 +1,41 @@
+package com.bootcoding.spring.coupon.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String title;
+    private String description;
+    @Column(name = "coupon_code")
+    private String couponCode;
+    private String type;
+    private String category;
+    @Column(name = "validity_date")
+    private Date validityDate;
+    @Column(name = "created_date")
+    private Date createdDate;
+    @Column(name = "created_by")
+    private String createdBy;
+    private String status;
+    @Column(name = "active")
+    private boolean isActive;
+
+}
