1608188772d67ab46d4906b74d15f08fb1dd835d
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-27 23:19:53 +0530

    Generator

--- coupon-system/coupon-system/src/main/java/com/bootcoding/spring/coupon/utils/TypeGenerator.java
+++ coupon-system/coupon-system/src/main/java/com/bootcoding/spring/coupon/utils/TypeGenerator.java
@@ -1,4 +1,14 @@
 package com.bootcoding.spring.coupon.utils;
 
+import com.bootcoding.spring.coupon.model.Coupon;
+
+import java.util.Random;
+
 public class TypeGenerator {
+    private static String[] type = {"Voucher", "Coupon"};
+
+    public static String getType() {
+        Random r = new Random();
+        return type[r.nextInt(0, type.length - 1)];
+    }
 }
