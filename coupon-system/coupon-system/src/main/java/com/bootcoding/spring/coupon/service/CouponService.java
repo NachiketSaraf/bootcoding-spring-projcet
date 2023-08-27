0873ca0209cd3dc005c99a66c4e8ea625f5b0161
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-27 23:15:18 +0530

    Coupon Service

---
+++ coupon-system/coupon-system/src/main/java/com/bootcoding/spring/coupon/service/CouponService.java
@@ -0,0 +1,55 @@
+package com.bootcoding.spring.coupon.service;
+
+import com.bootcoding.spring.coupon.model.Coupon;
+import com.bootcoding.spring.coupon.repository.CouponRepository;
+import com.bootcoding.spring.coupon.utils.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    public Coupon generateNewCoupon() {
+        Coupon coupon = Coupon.builder()
+                .isActive(ActiveGenerator.getCategories())
+                .title(TitleGenerator.getTitle())
+                .createdBy(CreatedByGenerator.getCreator())
+                .createdDate(CreatedDateGenerator.createdDate())
+                .couponCode(CouponCodeGenerator.generateNewCoupon())
+                .description(DescriptionGenerator.getDescription())
+                .category(CategoriesGenerator.getCategories())
+                .status(StatusGenerator.getStatus())
+                .type(TypeGenerator.getType())
+                .validityDate(ValidityDateGenerator.getValidity())
+                .build();
+        return couponRepository.save(coupon);
+    }
+
+    public List<Coupon> generateNewCoupon(int multi) {
+        List<Coupon> couponList = new ArrayList<>();
+        for (int i = 0; i < multi ; i++) {
+            couponList.add(generateNewCoupon());
+        }
+        couponRepository.saveAll(couponList);
+        return couponList;
+    }
+
+    public List<Coupon> getAllCoupon() {
+        return couponRepository.findAll();
+    }
+
+    public Optional<Coupon> getById(int id) {
+        return couponRepository.findById(id);
+    }
+
+    public void setCoupon(Coupon coupon) {
+        couponRepository.save(coupon);
+    }
+}
