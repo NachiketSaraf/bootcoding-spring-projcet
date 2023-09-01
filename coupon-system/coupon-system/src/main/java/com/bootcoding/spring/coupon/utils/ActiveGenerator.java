1598ed5f6ac014fb4b4f6562654b71a520116c37
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-01 23:34:12 +0530

    Active Generator

--- coupon-system/coupon-system/src/main/java/com/bootcoding/spring/coupon/utils/ActiveGenerator.java
+++ coupon-system/coupon-system/src/main/java/com/bootcoding/spring/coupon/utils/ActiveGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ActiveGenerator {
     private static boolean[] active={true,false};
-    public static boolean getCategories(){
+    public static boolean getActive(){
         Random random = new Random();
         return active[random.nextInt(0,active.length-1)];
     }
